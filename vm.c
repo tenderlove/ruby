b83829bfdf6346af835e93a95c9a3d11eaeed02c
Aaron Patterson <tenderlove@ruby-lang.org>
2022-02-28 14:36:51 -0800

    fix type so comparisons work

--- vm.c
+++ vm.c
@@ -3598,7 +3598,7 @@ vm_disable_inlining(VALUE self)
 static VALUE
 vm_inline_threshold_set(VALUE self, VALUE n)
 {
-    inline_threshold = NUM2INT(n);
+    inline_threshold = NUM2ULL(n);
     return n;
 }
 
