80c22ba7b1643a5f5b782b3ae59c9a991cc12158
Jemma Issroff <jemmaissroff@gmail.com>
2022-09-22 14:23:38 -0400

    Revert guarding on mjit_compiler

--- lib/mjit/compiler.rb
+++ lib/mjit/compiler.rb
@@ -354,41 +354,44 @@ module RubyVM::MJIT
         src << "{\n"
         src << "    VALUE obj = GET_SELF();\n"
         src << "    const shape_id_t source_shape_id = (rb_serial_t)#{ic_copy.source_shape_id};\n"
-        # JIT: cache hit path of vm_getivar/vm_setivar, or cancel JIT (recompile it with exivar)
-        if insn_name == :setinstancevariable
-          src << "    const uint32_t index = #{ic_copy.attr_index - 1};\n"
-          src << "    const shape_id_t dest_shape_id = (rb_serial_t)#{ic_copy.dest_shape_id};\n"
-          src << "    if (LIKELY(RB_TYPE_P(obj, T_OBJECT) && source_shape_id == ROBJECT_SHAPE_ID(obj))) {\n"
-          src << "        if (dest_shape_id != ROBJECT_SHAPE_ID(obj)) {\n"
-          src << "           if (UNLIKELY(index >= ROBJECT_NUMIV(obj))) {\n"
-          src << "               rb_init_iv_list(obj);\n"
-          src << "           }\n"
-          src << "           ROBJECT_SET_SHAPE_ID(obj, dest_shape_id);\n"
-          src << "        }\n"
-          src << "        VALUE *ptr = ROBJECT_IVPTR(obj);\n"
-          src << "        RB_OBJ_WRITE(obj, &ptr[index], stack[#{stack_size - 1}]);\n"
-          src << "    }\n"
-        else
-          if ic_copy.attr_index == 0 # cache hit, but uninitialized iv
-            src << "    /* Uninitialized instance variable */\n"
-            src << "    if (LIKELY(RB_TYPE_P(obj, T_OBJECT) && source_shape_id == ROBJECT_SHAPE_ID(obj))) {\n"
-            src << "        stack[#{stack_size}] = Qnil;\n"
+        if status.merge_ivar_guards_p
+          # JIT: Access ivar without checking these VM_ASSERTed prerequisites as we checked them in the beginning of `mjit_compile_body`
+          src << "    VM_ASSERT(RB_TYPE_P(obj, T_OBJECT));\n"
+          # JIT: cache hit path of vm_getivar/vm_setivar, or cancel JIT (recompile it with exivar)
+          if insn_name == :setinstancevariable
+            src << "    const uint32_t index = #{ic_copy.attr_index - 1};\n"
+            src << "    const shape_id_t dest_shape_id = (rb_serial_t)#{ic_copy.dest_shape_id};\n"
+            src << "    if (source_shape_id == ROBJECT_SHAPE_ID(obj)) {\n"
+            src << "        if (dest_shape_id != ROBJECT_SHAPE_ID(obj)) {\n"
+            src << "           if (UNLIKELY(index >= ROBJECT_NUMIV(obj))) {\n"
+            src << "               rb_init_iv_list(obj);\n"
+            src << "           }\n"
+            src << "           ROBJECT_SET_SHAPE_ID(obj, dest_shape_id);\n"
+            src << "        }\n"
+            src << "        VALUE *ptr = ROBJECT_IVPTR(obj);\n"
+            src << "        RB_OBJ_WRITE(obj, &ptr[index], stack[#{stack_size - 1}]);\n"
             src << "    }\n"
           else
-            src << "    VALUE val;\n"
-            src << "    const uint32_t index = #{ic_copy.attr_index - 1};\n"
-            src << "    if (LIKELY(RB_TYPE_P(obj, T_OBJECT) && source_shape_id == ROBJECT_SHAPE_ID(obj) && index < ROBJECT_NUMIV(obj) && (val = ROBJECT_IVPTR(obj)[index]) != Qundef)) {\n"
-            src << "        stack[#{stack_size}] = val;\n"
-            src << "    }\n"
+            if ic_copy.attr_index == 0 # cache hit, but uninitialized iv
+              src << "    /* Uninitialized instance variable */\n"
+              src << "    if (source_shape_id == ROBJECT_SHAPE_ID(obj))) {\n"
+              src << "        stack[#{stack_size}] = Qnil;\n"
+              src << "    }\n"
+            else
+              src << "    const uint32_t index = #{ic_copy.attr_index - 1};\n"
+              src << "    if (source_shape_id == ROBJECT_SHAPE_ID(obj) && index < ROBJECT_NUMIV(obj)) {\n"
+              src << "        stack[#{stack_size}] = ROBJECT_IVPTR(obj)[index];\n"
+              src << "    }\n"
+            end
           end
+          src << "    else {\n"
+          src << "        reg_cfp->pc = original_body_iseq + #{pos};\n"
+          src << "        reg_cfp->sp = vm_base_ptr(reg_cfp) + #{stack_size};\n"
+          src << "        goto ivar_cancel;\n"
+          src << "    }\n"
+          src << "}\n"
+          return src
         end
-        src << "    else {\n"
-        src << "        reg_cfp->pc = original_body_iseq + #{pos};\n"
-        src << "        reg_cfp->sp = vm_base_ptr(reg_cfp) + #{stack_size};\n"
-        src << "        goto ivar_cancel;\n"
-        src << "    }\n"
-        src << "}\n"
-        return src
       elsif insn_name == :getinstancevariable && !status.compile_info.disable_exivar_cache && ic_copy.source_shape_id != C.INVALID_SHAPE_ID
         # JIT: optimize away motion of sp and pc. This path does not call rb_warning() and so it's always leaf and not `handles_sp`.
         # compile_pc_and_sp(src, insn, stack_size, sp_inc, local_stack_p, next_pos)
@@ -400,9 +403,8 @@ module RubyVM::MJIT
         src << "    const uint32_t index = #{ic_copy.attr_index - 1};\n"
         # JIT: cache hit path of vm_getivar, or cancel JIT (recompile it without any ivar optimization)
         src << "    struct gen_ivtbl *ivtbl;\n"
-        src << "    VALUE val;\n"
-        src << "    if (LIKELY(FL_TEST_RAW(obj, FL_EXIVAR) && source_shape_id == rb_shape_get_shape_id(obj) && rb_ivar_generic_ivtbl_lookup(obj, &ivtbl) && index < ivtbl->numiv && (val = ivtbl->ivptr[index]) != Qundef)) {\n"
-        src << "        stack[#{stack_size}] = val;\n"
+        src << "    if (LIKELY(FL_TEST_RAW(obj, FL_EXIVAR) && source_shape_id == rb_shape_get_shape_id(obj) && rb_ivar_generic_ivtbl_lookup(obj, &ivtbl) && index < ivtbl->numiv) {\n"
+        src << "        stack[#{stack_size}] = ivtbl->ivptr[index];\n"
         src << "    }\n"
         src << "    else {\n"
         src << "        reg_cfp->pc = original_body_iseq + #{pos};\n"
@@ -801,6 +803,17 @@ module RubyVM::MJIT
 
     def init_ivar_compile_status(body, status)
       C.mjit_capture_is_entries(body, status.is_entries)
+
+      pos = 0
+
+      while pos < body.iseq_size
+        insn = INSNS.fetch(C.rb_vm_insn_decode(body.iseq_encoded[pos]))
+        if insn.name == :getinstancevariable || insn.name == :setinstancevariable
+          status.merge_ivar_guards_p = true
+          return
+        end
+        pos += insn.len
+      end
     end
 
     # Expand simple macro that doesn't require dynamic C code.
