1df55d6eda146d16838e7f4570f78e03dd6bab7a
Jemma Issroff <jemmaissroff@gmail.com>
2023-08-28 11:48:21 -0700

    Added yarp/ast.h as dependency of yarp/yarp.h

--- common.mk
+++ common.mk
@@ -222,6 +222,8 @@ srcs: yarp/api_node.c
 yarp/api_node.c: $(YARP_SRCDIR)/config.yml $(YARP_SRCDIR)/templates/template.rb $(YARP_SRCDIR)/templates/ext/yarp/api_node.c.erb
 	$(Q) $(BASERUBY) $(YARP_SRCDIR)/templates/template.rb ext/yarp/api_node.c $@
 
+yarp/yarp.h: yarp/ast.h
+
 srcs: yarp/ast.h
 yarp/ast.h: $(YARP_SRCDIR)/config.yml $(YARP_SRCDIR)/templates/template.rb $(YARP_SRCDIR)/templates/include/yarp/ast.h.erb
 	$(Q) $(BASERUBY) $(YARP_SRCDIR)/templates/template.rb include/yarp/ast.h $@
